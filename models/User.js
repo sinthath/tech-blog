26e29d3f907c74b9768c77a47e47569e4cdf31dd
sinthath <sinthathirath@gmail.com>
2022-04-22 10:46:33 -0400

    set up models

---
+++ models/User.js
@@ -0,0 +1,58 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+const bcrypt = require('bcrypt');
+
+class User extends Model {
+  //set up method to check password
+  checkPassword(loginPw) {
+    return bcrypt.compareSync(loginPw, this.password);
+  }
+}
+
+User.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    username: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [5]
+      }
+    }
+  },
+  {
+    hooks: {
+      // use beforeCreate for hook functionality
+      async beforeCreate(newUserData) {
+        newUserData.password = await bcrypt.hash(newUserData.password, 10);
+        return newUserData;
+      },
+      async beforeUpdate(updatedUserData) {
+        updatedUserData.password = await bcrypt.hash(updatedUserData.password, 10);
+        return updatedUserData;
+      }
+    },  
+    //pass in the imported sequelize connection
+    sequelize,
+    // don't automatically create timestamp fields
+    timestamps: false,
+    // don't plurallize name of database table
+    freezeTableName: true,
+    // use underscores instead of camel-casing
+    underscored: true,
+    // make it so our model name stays in lowercase in the database
+    modelName: 'user'
+  }
+);
+
+module.exports = User;
