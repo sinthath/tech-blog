752355dff720477b9eb0cb81462751c16edfa10d
sinthath <sinthathirath@gmail.com>
2022-04-22 10:58:28 -0400

    login and sign up handler

--- public/javascript/login.js
+++ public/javascript/login.js
@@ -49,5 +49,6 @@ async function signupFormHandler(event) {
     }
   }
   
+  
   document.querySelector('.signup-form').addEventListener('submit', signupFormHandler);
   document.querySelector('.login-form').addEventListener('submit', loginFormHandler);
